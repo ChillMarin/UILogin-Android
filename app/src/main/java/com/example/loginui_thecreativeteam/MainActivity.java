9f6f2e6df5a99543eeedc24a7d2296b7d564edd8
Marcel Marin <marcelmarin256@gmail.com>
2022-12-15 11:00:21 -0400

    Funciona el logi, logput, register and reset password

--- app/src/main/java/com/example/loginui_thecreativeteam/MainActivity.java
+++ app/src/main/java/com/example/loginui_thecreativeteam/MainActivity.java
@@ -2,13 +2,70 @@ package com.example.loginui_thecreativeteam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edEmail, edPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+
+        if(ParseUser.getCurrentUser()!=null){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void login (View view){
+        //Validamos si el input del email esta vacio
+        if (TextUtils.isEmpty(edEmail.getText())){
+            edEmail.setError("Email requerido");
+        }else if (TextUtils.isEmpty(edPassword.getText())){
+            edPassword.setError("Password requerido");
+        }else {
+            final ProgressDialog progress = new ProgressDialog(this);
+            progress.setMessage("Loading ...");
+            progress.show();
+            ParseUser.logInInBackground(edEmail.getText().toString(), edPassword.getText().toString(), (parseUser, e) -> {
+                if (parseUser != null) {
+                    Toast.makeText(MainActivity.this, "Welcome back",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    progress.dismiss();
+                    ParseUser.logOut();
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+    public void forgotPassword(View view) {
+        Intent intent = new Intent(MainActivity.this, ResetPassword.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/loginui_thecreativeteam/ResetPassword.java
@@ -0,0 +1,43 @@
+package com.example.loginui_thecreativeteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
+
+public class ResetPassword extends AppCompatActivity {
+
+    EditText edEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+        edEmail = findViewById(R.id.edEmail);
+    }
+
+    public void resetPassword(View view){
+        if (TextUtils.isEmpty(edEmail.getText())) {
+            edEmail.setError("Email requerido");
+        }
+        ParseUser.requestPasswordResetInBackground(edEmail.getText().toString(), new RequestPasswordResetCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // An email was successfully sent with reset instructions.
+                    Toast.makeText(ResetPassword.this, "Email enviado correctamente", Toast.LENGTH_LONG).show();
+                } else {
+                    // Something went wrong. Look at the ParseException to see what's up.
+                    Toast.makeText(ResetPassword.this, "A ocurrido un error", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+}
